b9a6cde41efc9a0bb732579e56d2a8369df9f85a
EcemNazGorusuk <ecemnazgorusuk@gmail.com>
2024-05-03 14:33:57 +0300

    logo created

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-[#F9F9F9]">
+      <h1>
+        <span className="text-[#76BA99] font-[450]">Mercatus Immobilis</span>
+        <span className="text-[#FF7F3F] font-[700]"> Novatorum</span>
+      </h1>
+    </header>
+  );
+}
