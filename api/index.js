8705b9d93536596731534691487628f7b4897677
EcemNazGorusuk <ecemnazgorusuk@gmail.com>
2024-05-10 13:46:06 +0300

    connected to mongodb!

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
-mongoose.connect(process.env.MONGO);
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDb!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
