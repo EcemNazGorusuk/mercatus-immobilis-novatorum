f072d01b334b2249bf7c925ce8b5b5a72c24ff79
EcemNazGorusuk <ecemnazgorusuk@gmail.com>
2024-05-10 13:34:42 +0300

    connected to db!

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-const app=express();
+dotenv.config();
+mongoose.connect(process.env.MONGO);
 
+const app = express();
 
-app.listen(3000,()=>{
-console.log(`server is running on port 3000!`)
-})
+app.listen(3000, () => {
+  console.log(`server is running on port 3000!`);
+});
