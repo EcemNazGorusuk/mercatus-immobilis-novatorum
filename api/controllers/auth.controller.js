1dc51a21b2b462344e23838e275992b781470e1f
EcemNazGorusuk <ecemnazgorusuk@gmail.com>
2024-05-14 14:57:15 +0300

    signin controller is done.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 //register
 export const signupController = async (req, res, next) => {
@@ -25,17 +26,31 @@ export const signinController = async (req, res, next) => {
   console.log("signin request : ", req.body);
 
   const { email, password } = req.body;
-  //hash password
-  //const hashedPassword = await bcryptjs.hashSync(password, 12);
 
   try {
     const validUser = await User.findOne({ email }); //Check user exists.
+    //can access this property like this-> validUser.email || validUser.password
+
     if (!validUser) {
       return next(errorHandler(404, "User not found!")); //404-> not found
     }
-   
+    //hash & compare password
+    const hashedValidPassword =  bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+
+    if (!hashedValidPassword) {
+      return next(errorHandler(401, "Wrong credential - invalid password!")); //401-> not match || invalid
+    }
+    //token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //hide bcrypted password after post process 
+    const { password: pass, ...rest } = validUser._doc;
+    //generate cookie
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+
   } catch (error) {
-    //res.status(500).json(error.message);
     next(errorHandler(500, error.message)); //500-> error
   }
 };
